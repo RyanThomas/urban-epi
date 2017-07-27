108cff4d2a519be043495c830e3fb396061b02fd
ryanthomas <ryan.thomas.rt@gmail.com>
2017-07-27 17:12:26 -0400

    add sbatch to header

--- bash/write_jobs.sh
+++ bash/write_jobs.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 for CITY in $SEED/*.shp; do 
-echo  bash  source/bash/task_manager.sh '$GRASSDB' location $CITY ; 
+echo  source/bash/task_manager.sh '$GRASSDB' location $CITY ; 
 done > source/bash/job_list.txt
