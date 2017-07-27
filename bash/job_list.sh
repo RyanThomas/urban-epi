744a75001e2f079a7fb82b5c4993854445f4880f
ryanthomas <ryan.thomas.rt@gmail.com>
2017-07-27 17:13:38 -0400

    getting to work in cluster

---
+++ bash/job_list.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#SBATCH --partition=general
+#SBATCH --job-name=uepi
+#SBATCH --ntasks=12 --nodes=12
+#SBATCH --mem-per-cpu=8000 
+#SBATCH --time=12:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=email
+
+source/bash/task_manager.sh $GRASSDB location /home/user/projects/urban_epi/source/seed_data/beijing.shp
+source/bash/task_manager.sh $GRASSDB location /home/user/projects/urban_epi/source/seed_data/delhi.shp
+source/bash/task_manager.sh $GRASSDB location /home/user/projects/urban_epi/source/seed_data/hochiminh.shp
+source/bash/task_manager.sh $GRASSDB location /home/user/projects/urban_epi/source/seed_data/jakarta.shp
+source/bash/task_manager.sh $GRASSDB location /home/user/projects/urban_epi/source/seed_data/london.shp
+source/bash/task_manager.sh $GRASSDB location /home/user/projects/urban_epi/source/seed_data/losangeles.shp
+source/bash/task_manager.sh $GRASSDB location /home/user/projects/urban_epi/source/seed_data/manila.shp
+source/bash/task_manager.sh $GRASSDB location /home/user/projects/urban_epi/source/seed_data/mexico.shp
+source/bash/task_manager.sh $GRASSDB location /home/user/projects/urban_epi/source/seed_data/newyork.shp
+source/bash/task_manager.sh $GRASSDB location /home/user/projects/urban_epi/source/seed_data/saopaulo.shp
+source/bash/task_manager.sh $GRASSDB location /home/user/projects/urban_epi/source/seed_data/seoul.shp
+source/bash/task_manager.sh $GRASSDB location /home/user/projects/urban_epi/source/seed_data/tokyo.shp
