2d757c4db87cefddad8ee9f2a5bb6176b54c5755
ryanthomas <ryan.thomas.rt@gmail.com>
2017-07-28 13:28:14 -0400

    modified data directory location

--- bash/clip_landuse.sh
+++ bash/clip_landuse.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-#SBATCH --partition=general
-#SBATCH --job-name=clip_landuse
-#SBATCH --ntasks=12 
-#SBATCH --cpus-per-task=2
-#SBATCH --mem-per-cpu=8000 
-#SBATCH --time=12:00:00
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=ryan.thomas@yale.edu
+export DIR=$HOME
+export DATA=project/urban_epi/data 
+export IND="${DIR}/indicators"
+export SH="${DIR}/source/bash" 
+export GRASSDB="${DIR}/grassdb" 
+export RAS="${DATA}/raster"    
+export VEC="${DATA}/vector"   
+export SEED="${DIR}/source/seed_data"
 
-for CITY in $SEED/*.shp; do
-NAME=$(echo `basename "$CITY"` | awk -F '.' '{ print $1 }')
+export NAME=$(echo `basename "$1"` | awk -F '.' '{ print $1 }')
 # Create bounds variable for g.region
 # Create bounds for gdalwarp +2 degree buffer
-export gwarpBOUNDS=$(ogrinfo -al -so $CITY  | grep "Extent: " | \
+export gwarpBOUNDS=$(ogrinfo -al -so $1  | grep "Extent: " | \
     awk  '{ gsub ("[(),-]", ""); print ($2-2" "$3-2" "$4+2" "$5+2) }' ) 
 
 echo "Writing  $RAS/glcf/landuse_cover_${NAME}.tif"
@@ -21,13 +20,12 @@ echo "Writing  $RAS/glcf/landuse_cover_${NAME}.tif"
 gdalwarp -te $gwarpBOUNDS -r average -multi -wo NUM_THREADS=2 -overwrite \
     $RAS/glcf/landuse_cover.vrt \
     $RAS/glcf/landuse_cover_${NAME}.tif 
-# AIR QUALITY 2014
+echo  AIR QUALITY 2014
 #TODO: Make these air files into VRT.
 gdalwarp  -te $gwarpBOUNDS -r average -multi -wo NUM_THREADS=2 -overwrite \
     $RAS/pm25/GlobalGWR_PM25_GL_201401_201412-RH35_NoDust_NoSalt-NoNegs.asc \
     $RAS/air_pm25_2014_${NAME}.tif 
-# AIR QUALITY 2015
+echo  AIR QUALITY 2015
 gdalwarp  -te $gwarpBOUNDS -r average -multi -wo NUM_THREADS=2 -overwrite \
     $RAS/pm25/GlobalGWR_PM25_GL_201501_201512-RH35_NoDust_NoSalt-NoNegs.asc \
-    $RAS/air_pm25_2015_${NAME}.tif ; done
-    
+    $RAS/air_pm25_2015_${NAME}.tif     
